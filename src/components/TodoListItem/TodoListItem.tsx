de4e34d3ec1f201e586944d04e88a3cde2a35a86
Hanna Torianyk <an.mol4anova@gmail.com>
2026-01-27 18:45:03 +0200

    Solution

---
+++ src/components/TodoListItem/TodoListItem.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Todo } from '../../types/Todo';
+
+type Props = {
+  todo: Todo;
+  isSelected: boolean;
+  selectTodo: () => void;
+};
+
+export const TodoListItem: React.FC<Props> = ({
+  todo,
+  isSelected,
+  selectTodo,
+}) => {
+  return (
+    <tr data-cy="todo">
+      <td className="is-vcentered">{todo.id}</td>
+
+      <td className="is-vcentered">
+        {todo.completed && (
+          <span className="icon has-text-success" data-cy="iconCompleted">
+            <i className="fas fa-check" />
+          </span>
+        )}
+      </td>
+
+      <td className="is-vcentered is-expanded">
+        <p className={todo.completed ? 'has-text-success' : 'has-text-danger'}>
+          {todo.title}
+        </p>
+      </td>
+
+      <td className="has-text-right is-vcentered">
+        {!isSelected ? (
+          <button
+            data-cy="selectButton"
+            className="button"
+            type="button"
+            onClick={selectTodo}
+          >
+            <span className="icon">
+              <i className="far fa-eye" />
+            </span>
+          </button>
+        ) : (
+          <button
+            data-cy="unselectButton"
+            className="button"
+            type="button"
+            onClick={selectTodo}
+          >
+            <span className="icon">
+              <i className="far fa-eye-slash" />
+            </span>
+          </button>
+        )}
+      </td>
+    </tr>
+  );
+};
